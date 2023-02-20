f47b351c0beef8fc21f739f3a88a2813e3e3bd6b
Varun Rao Bhamidimarri <vbhamidi@amazon.com>
2023-02-19 21:08:03 -0800

    Change to a explicit "True"for skip_missing_names_on_install

--- aws_emr_blog_v3/scripts/remove-yum-package-name-validator.sh
+++ aws_emr_blog_v3/scripts/remove-yum-package-name-validator.sh
@@ -1,1 +1,1 @@
-sudo sed -ie "s#--yum-config-option skip_missing_names_on_install=False ##g" /usr/share/aws/emr/node-provisioner/bin/provision-node
+sudo sed -ie "s#--yum-config-option skip_missing_names_on_install=False #--yum-config-option skip_missing_names_on_install=True #g" /usr/share/aws/emr/node-provisioner/bin/provision-node
